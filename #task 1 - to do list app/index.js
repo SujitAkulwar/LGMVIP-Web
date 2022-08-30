a9fa534dbae8e15fcdebcb826d9837ae49a3c4f6
Sujit Akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2022-08-30 11:03:28 +0530

    2

--- #task 1 - to do list app/index.js
+++ #task 1 - to do list app/index.js
@@ -1,24 +1,15 @@
+var submit = document.getElementById('add');
+var task = document.getElementById("list");
+submit.addEventListener("click", add);
 
-document.querySelector('#add').onclick = function(){
-    if(document.querySelector('#new').value.length == 0){
-        alert("Please Enter a Task")
-    }else{
-        document.querySelector('#task').innerHTML += `
-        <div id="tasks">
-            <div id="task-name">
-                ${document.querySelector('#new').value}
-            </div>
-            <button id="delete">
-                X
-            </button>
-        </div>
-        `;
-
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
+function add(){
+    var text = document.querySelector('input').value;
+    if(text == null){
+        alert('empty task');
     }
+    else{
+        console.log(text);
+        task.innerHTML += 
+    }
+    
 }
