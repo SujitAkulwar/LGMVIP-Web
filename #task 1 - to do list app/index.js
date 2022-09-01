2ca572a14d686309d17bc6a0740cf7bb9531de1d
Sujit Akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2022-09-01 09:06:49 +0530

    1

--- #task 1 - to do list app/index.js
+++ #task 1 - to do list app/index.js
@@ -1,14 +1,17 @@
 var submit = document.getElementById('add');
-var task = document.getElementById("list");
+var list = document.getElementById("list");
 submit.addEventListener("click", add);
 
 function add(){
     var text = document.querySelector('input').value;
-    if(text == null){
+    if(text == ""){
         alert('empty task');
     }
     else{
         console.log(text);
+        
+        
+        
     }
     
 }
