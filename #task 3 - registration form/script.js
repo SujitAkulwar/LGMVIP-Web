84a38c3fc509954d06f93e885eff670f92e0abb4
Sujit Akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2022-09-03 11:42:12 +0530

    2

---
+++ #task 3 - registration form/script.js
@@ -0,0 +1,13 @@
+function formValidation(){
+    console.log("hello");
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var email = document.getElementById("email").value;
+    var email = document.getElementById("email").value;
+    console.log(name);
+
+}
+
+function addphoto(){
+
+}
