29af2e76b7b271f367294cfd5a64f680fe44739c
Sujit Akulwar <85801946+SujitAkulwar@users.noreply.github.com>
2022-09-04 13:02:06 +0530

    1

--- #task 3 - registration form/script.js
+++ #task 3 - registration form/script.js
@@ -1,12 +1,44 @@
+var gender = null;
+var cpp="",python="",java="";
+var c1=0,c2=0,c3=0;
+function setcpp(){
+    c1++;
+    if(c1%2==1){
+        cpp="c++";
+    }else{
+        cpp="";
+    }
+}
+
+function setjava(){
+    c2++;
+    if(c2%2==1){
+        java="java";
+    }else{
+        java="";
+    }
+}
+
+function setpython(){
+    c3++;
+    if(c3%2==1){
+        python="python";
+    }else{
+        python="";
+    }
+}
+
+function setgender(x){
+    gender = x;
+}
 function formValidation(){
     console.log("hello");
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var website = document.getElementById("website").value;
-    var gender = document.getElementById("gender").value;
-    var skill = document.getElementById("skill").value;
+    var skill = cpp+" "+java+" "+python;
     console.log(name,email,website,gender,skill);
-
+    
 }
 
 function addphoto(){
